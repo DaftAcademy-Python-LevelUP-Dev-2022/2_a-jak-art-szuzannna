7d443305a421073f4eb4720922d9a199f3553647
szuzannna <105456200+szuzannna@users.noreply.github.com>
2022-05-23 23:18:37 +0200

    task2

--- file.py
+++ file.py
@@ -7,7 +7,32 @@ def greeter(func):
 
 
 def sums_of_str_elements_are_equal(func):
-    pass
+    def comaprison(*args,**kwargs):
+        str_of_numbers = func(*args, **kwargs)
+        list_of_numbers =[]
+        list_of_strings = str_of_numbers.split()
+        for string_ in list_of_strings:
+            result = 0
+            for char in string_:
+                if char == "-":
+                    minus = -1
+                    continue
+                char = int(char)
+                try:
+                    char = char * minus
+                except NameError:
+                    pass
+                result += char
+            list_of_numbers.append(result)
+            try:
+                del minus
+            except UnboundLocalError:
+                pass
+        if list_of_numbers[0] == list_of_numbers[1]:
+            return "" + list_of_numbers[0]+" == " + list_of_numbers[1]
+        else:
+            return "" + list_of_numbers[0]+" != " + list_of_numbers[1]
+    return comaprison
 
 
 def format_output(*required_keys):
